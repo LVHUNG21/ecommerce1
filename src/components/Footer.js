ea3e38e96922c5696c9a3a8a0bc73656d979a550
LeVanHung34554 <hung0911248587@gmail.com>
2023-03-03 14:57:53 +0700

    huing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,51 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-     footer 
-    </div>
+    <>
+      <footer className='py-4'>
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-5">
+              <div className="footer-top-data d-flex gap 30 align-items-center">
+                <img src='imgages/newsletter.png' alt='newletter' />
+                <h2 className='mb-0 text-white'>
+                  Sign Up for Newsletter
+                </h2>
+              </div>
+            </div>
+            <div className="col-7">
+              <div className="input-group ">
+                <input type="text" className="form-control py-1"
+                  placeholder="Your email address"
+                  aria-label="Your email address"
+                  aria-describedby="basic-addon2" />
+                <span className="input-group-text p-2" id="basic-addon2">
+
+                Subscribe 
+                </span>
+              </div>
+            </div>
+            </div>
+          </div>
+
+      </footer>
+      <footer className='py-3'>
+
+      </footer>
+      <footer className='py-4'>
+        <div className='container-xxl'>
+          <div row>
+            <div className='col-12'>
+              <p className='text-center mb-0 text-white'> &copy;
+                {new Date().getFullYear()};Powered by Developer's Corner('')
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+
+    </>
   )
 }
 
