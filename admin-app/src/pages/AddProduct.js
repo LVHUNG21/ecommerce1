e4a307cfca9394a28ead79dcdbe69e6d68b65cd6
Sinh Vien TLU ( Pass:123456 ) <hung0911248587@gmail.com>
2023-03-16 16:08:39 +0700

    16:08

--- admin-app/src/pages/AddProduct.js
+++ admin-app/src/pages/AddProduct.js
@@ -1,30 +1,46 @@
-import { React, useState } from 'react'
+import { React, useEffect, useState } from 'react'
 import CustomInput from './CustomInput'
-import {InboxOutlined} from "@ant-design/icons"
+import { InboxOutlined } from "@ant-design/icons"
 import ReactQuill from 'react-quill';
-import { Upload ,message} from 'antd';
+import { Upload, message } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import {getBrands} from '../features/brand/brandSlice';
 import "react-quill/dist/quill.snow.css";
 const { Dragger } = Upload;
-const props = {
-    name: 'file',
-    multiple: true,
-    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-    onChange(info) {
-        const { status } = info.file;
-        if (status !== 'uploading') {
-            console.log(info.file, info.fileList);
-        }
-        if (status === 'done') {
-            message.success(`${info.file.name} file uploaded successfully.`);
-        } else if (status === 'error') {
-            message.error(`${info.file.name} file upload failed.`);
-        }
-    },
-    onDrop(e) {
-        console.log('Dropped files', e.dataTransfer.files);
-    },
-};
+let userSchema = Yup.object({
+    title: Yup.string().required("tite is Required"),
+    description: Yup.string().required("Description is Required"),
+    price: Yup.string().required("pRICE IS REQUIRED"),
+    category: Yup.string().required("category is required"),
+    color: Yup.string().required('color is required')
+});
 const AddProduct = () => {
+    const dispatch=useDispatch();
+
+    useEffect(()=>{
+        dispatch(getBrands());
+    },[])
+    const brandState=useSelector((state)=>state.brand.brands);
+  
+    const formik = useFormik({
+        initialValues: {
+            title: '',
+            description: '',
+            price: '',
+            category: '',
+            color: '',
+        },
+        validationSchema: userSchema,
+        onSubmit: (values) => {
+            alert(JSON.stringify(values))
+            // alert(JSON.stringify(values, null, 2));
+        },
+    });
+    const { user, isLoading, isError, isSuccess, message } = useSelector(
+        (state) => state.auth
+    );
     const [desc, setDesc] = useState();
     const handleDesc = (e) => {
         setDesc(e);
@@ -33,17 +49,40 @@ const AddProduct = () => {
     }
     return (
         <div>
-            <h3 className="mb-4title">AddProduct
+            <h3 className="mb-4 title">AddProduct
             </h3>
             <div>
-                <form>
-                    <CustomInput type='text' label='Enter Product title' />
-                    <div className='mb-3'>
-                        <ReactQuill theme="snow" value={desc} onChange={(evt) => {
-                            handleDesc(evt);
-                        }} />
+                <form onSubmit={formik.handleSubmit} className="d-flex gap-3 flex-column">
+                    <CustomInput name='title' onChng={formik.handleChange('title')} onBlr={formik.handleBlur('title')} type='text' label='Enter Product title' />
+                    <div className='error'>
+                        {
+                            formik.touched.title && formik.errors.title
+                        }
                     </div>
-                    <CustomInput type='text' label='Enter Product title' />
+                    <div className='mb-3'>
+                        <ReactQuill theme="snow"
+
+                            name="description"
+                            onChng={formik.handleChange('description')}
+                            onBlr={formik.handleBlur('description')}
+                            value={formik.values.description} />
+
+                        <div className='error'>
+                            {
+                                formik.touched.description && formik.errors.description
+                            }
+                        </div>
+                    </div>
+                    <CustomInput type='text' label='Enter Product price'
+                     name="price"
+                            onChng={formik.handleChange('price')}
+                            onBlr={formik.handleBlur('price')}
+                            value={formik.values.price}  />
+                    <div className='error'>
+                        {
+                            formik.touched.price && formik.errors.price
+                        }
+                    </div>
                     <select className="form-control py-3 mb-3" name="" id="">
                         <option value="">
                             Select Category
@@ -58,17 +97,15 @@ const AddProduct = () => {
                         <option value="">
                             Select Brand
                         </option>
+                        {brandState.map((i,j)=>{
+                            return (
+                                <option key={j} value={i.title}>
+                                    {i.title}
+                                </option>
+                            );
+                        })}
                     </select>
-                    <Dragger {...props}>
-                        <p className="ant-upload-drag-icon">
-                            <InboxOutlined />
-                        </p>
-                        <p className="ant-upload-text">Click or drag file to this area to upload</p>
-                        <p className="ant-upload-hint">
-                            Support for a single or bulk upload. Strictly prohibit from uploading company data or other
-                            band files
-                        </p>
-                    </Dragger>
+
                     <button type="submit" className='btn btn-success border-0 rounded-3 my-5'>
                         Add Brand
                     </button>
